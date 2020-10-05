e8bcac9c27a6dd158a212e246221de8bb9a5f8f0
a <myhmmngbrd@gmail.com>
2020-10-05 17:37:31 +0900

    b

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+
+const server = http.createServer(async (req, res) => {
+    try {    
+        const rootdir = __dirname;
+        const sourcedir = path.join(rootdir, 'public');
+        const sourcefiles = await fs.readdir(sourcedir);
+
+        for (const file in sourcefiles) {
+            console.log(file)
+        }
+
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+server.listen(3000, () => {
+    console.log('server is running...')
+})
